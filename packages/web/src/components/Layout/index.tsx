ea76c66d099179d5b9b614e1434c5ab4aafbbcb3
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-01-13 18:03:57 +0900

    Implement Header menu

--- packages/web/src/components/Layout/index.tsx
+++ packages/web/src/components/Layout/index.tsx
@@ -4,31 +4,25 @@ import { Layout } from 'antd';
 import { AppBar } from '../AppBar';
 import { Footer } from '../Footer';
 
-const { Header, Content } = Layout;
+const { Content } = Layout;
 
 export const AppLayout = React.memo((props: any) => {
   return (
-    <>
-      <Layout id={'main-layout'}>
-        <span id={'main-bg'}></span>
-        <span id={'bg-gradient'}></span>
-        <span id={'static-header-gradient'}></span>
-        <span id={'static-end-gradient'}></span>
-        <Header className="App-Bar">
-          <AppBar />
-        </Header>
-        <Layout id={'width-layout'}>
-          <Content
-            style={{
-              overflow: 'scroll',
-              padding: '30px 48px ',
-            }}
-          >
-            {props.children}
-          </Content>
-        </Layout>
-        {/*<Footer />*/}
+    <div className="main page">
+      <header id='header'>
+        <AppBar />
+      </header>
+      <Layout id={'width-layout'}>
+        <Content
+          style={{
+            overflow: 'scroll',
+            padding: '30px 48px ',
+          }}
+        >
+          {props.children}
+        </Content>
       </Layout>
-    </>
+      {/*<Footer />*/}
+    </div>
   );
 });
