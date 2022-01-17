eca68c241a60cd2f38147c0db51bcaaa7cc3ec4d
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-01-17 10:06:45 +0900

    Fixed css issue and updated ClickToCopy component

--- packages/web/src/components/ClickToCopy/index.tsx
+++ packages/web/src/components/ClickToCopy/index.tsx
@@ -1,57 +1,14 @@
 import React, { useEffect, useState } from 'react';
-
-const CopyIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    className="feather feather-copy"
-  >
-    <rect
-      stroke="currentColor"
-      x="9"
-      y="9"
-      width="13"
-      height="13"
-      rx="2"
-      ry="2"
-    ></rect>
-    <path
-      stroke="currentColor"
-      d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"
-    ></path>
-  </svg>
-);
-
-const Checkmark = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    className="feather feather-check"
-  >
-    <polyline points="20 6 9 17 4 12"></polyline>
-  </svg>
-);
+import { CheckOutlined, CopyOutlined } from "@ant-design/icons";
 
 export const ClickToCopy = ({
   copyText,
   className,
+  tooltip,
 }: {
   copyText: string;
   className: string;
+  tooltip: string;
 }) => {
   const [clicked, setClicked] = useState(false);
 
@@ -68,8 +25,8 @@ export const ClickToCopy = ({
   };
 
   return (
-    <div className={className} onClick={onClick} title="Click to copy pubkey">
-      {clicked ? <Checkmark /> : <CopyIcon />}
+    <div className={className} onClick={onClick} title={tooltip}>
+      {clicked ? <CheckOutlined style={{ color: "#00ffbd" }} /> : <CopyOutlined />}
     </div>
   );
 };
