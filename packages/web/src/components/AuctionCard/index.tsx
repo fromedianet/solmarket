98d1dc26f6a188f32dde4d58d18b20a0f7d95660
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-01-23 22:22:54 +0900

    Allowing claim, end and reclaim without enough balance

--- packages/web/src/components/AuctionCard/index.tsx
+++ packages/web/src/components/AuctionCard/index.tsx
@@ -363,9 +363,10 @@ export const AuctionCard = ({
     setShowEndingBidModal(true);
     setLoading(false);
   };
+  const { canEndInstantSale, isAlreadyBought, canClaimPurchasedItem, canClaimItem } = useInstantSaleState(auctionView);
   const instantSaleAction = () => {
     const isNotEnoughLamports = balance.balanceLamports < (instantSalePrice?.toNumber()  || 0)
-    if (isNotEnoughLamports) {
+    if (isNotEnoughLamports && !(canClaimPurchasedItem || canClaimItem || canEndInstantSale) ) {
       setShowFundsIssueModal(true);
       return;
     }
@@ -493,9 +494,6 @@ export const AuctionCard = ({
     shouldHideInstantSale ||
     (auctionView.vault.info.state === VaultState.Deactivated &&
       isBidderPotEmpty);
-
-  const { canEndInstantSale, isAlreadyBought, canClaimPurchasedItem } =
-    useInstantSaleState(auctionView);
 
   const actionButtonContent = useActionButtonContent(auctionView);
 
