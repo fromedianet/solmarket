d777023196f4f404511749964c22cdc110a2d0eb
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-02-01 00:26:57 +0900

    Implemented profile modal UI

--- packages/web/src/views/profile/index.tsx
+++ packages/web/src/views/profile/index.tsx
@@ -1,12 +1,16 @@
-import { ConnectButton, shortenAddress, useMeta } from "@oyster/common";
+import { ConnectButton, MetaplexModal, shortenAddress, useMeta } from "@oyster/common";
 import { useWallet } from "@solana/wallet-adapter-react";
-import React from "react";
-import { Button, Row, Col, Statistic, Tabs } from 'antd';
+import React, { useState } from "react";
+import { Button, Row, Col, Statistic, Tabs, Form, Input, message } from 'antd';
 import { CopySpan } from "../../components/CopySpan";
 
 const { TabPane } = Tabs;
+const { TextArea } = Input;
 
 export const ProfileView = () => {
+
+  const [visible, setVisible] = useState(false);
+  const [form] = Form.useForm();
 
   const wallet = useWallet();
   const { whitelistedCreatorsByCreator } = useMeta();
@@ -14,7 +18,16 @@ export const ProfileView = () => {
   if (wallet.connected) {
     item = whitelistedCreatorsByCreator[wallet.publicKey?.toBase58() || ''];
   }
-  console.log('item', item);
+  
+  const onSubmit = (values) => {
+    console.log(values);
+    message.success('Submit success!');
+  }
+
+  const onSubmitFailed = () => {
+    message.error("Submit failed!");
+  }
+
   return (
     <div className="main-area">
       <div className="profile-page">
@@ -32,7 +45,7 @@ export const ProfileView = () => {
                 {item.info.name && (<h1>{item.info.name}</h1>)}
                 <CopySpan value={shortenAddress(item.info.address, 8)} copyText={item.info.address} className='wallet-address'/>
                 {item.info.description && (<span className="description">{item.info.description}</span>)}
-                <Button className="profile-button">Edit Profile</Button>
+                <Button className="profile-button" onClick={() => setVisible(true)}>Edit Profile</Button>
               </div>
             ) : (
               <ConnectButton className="profile-button" />
@@ -62,6 +75,32 @@ export const ProfileView = () => {
           </Tabs>
         </div>
       </div>
+      <MetaplexModal visible={visible} onCancel={() => setVisible(false)} >
+        <div className="profile-modal">
+          <h1>Profile settings</h1>
+          <Form
+            form={form}
+            layout='vertical'
+            requiredMark='optional'
+            autoComplete="off"
+            onFinish={onSubmit}
+            onFinishFailed={onSubmitFailed}
+          >
+            <Form.Item name={['user', 'name']} label='Display name' required tooltip='This is a required field' rules={[{ required: true }]}>
+              <Input placeholder="Display name"/>
+            </Form.Item>
+            <Form.Item name={['user', 'description']} label='Description'>
+              <TextArea placeholder="Description" autoSize={{ minRows: 2, maxRows: 5 }}/>
+            </Form.Item>
+            <Form.Item name={['user', 'twitter']} label='Twitter'>
+              <Input addonBefore='https://' />
+            </Form.Item>
+            <Form.Item>
+              <Button htmlType="submit" className="submit-button">Submit</Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </MetaplexModal>
     </div>
   )
 }
