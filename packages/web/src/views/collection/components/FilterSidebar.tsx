6a8c3898408a6a3abbabf8061d1f1d841f78e825
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-02-05 03:05:37 +0900

    Filter price form

--- packages/web/src/views/collection/components/FilterSidebar.tsx
+++ packages/web/src/views/collection/components/FilterSidebar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Layout, Collapse } from 'antd';
+import { Layout, Collapse, Form, InputNumber, Space, Select, Button } from 'antd';
 import {
   ArrowLeftOutlined,
   ArrowRightOutlined,
@@ -12,6 +12,11 @@ const { Sider } = Layout;
 const { Panel } = Collapse;
 export const FilterSidebar = () => {
   const [collapsed, setCollapsed] = useState(false);
+
+  const onFinish = (values) => {
+    console.log(values);
+  }
+
   return (
     <Sider
       className="filter-sider"
@@ -49,7 +54,31 @@ export const FilterSidebar = () => {
               key="price"
               header="Price filter"
               extra={<UnorderedListOutlined className="filter-icon" />}
-            ></Panel>
+            >
+              <Form name='price_form' className='price-form' onFinish={onFinish} autoComplete='off'>
+                <Form.Item
+                  name={['price', 'symbol']}
+                >
+                  <Select defaultValue='sol' disabled>
+                    <Select.Option value='sol'>SOL</Select.Option>
+                  </Select>
+                </Form.Item>
+                <Space style={{ display: 'flex', justifyContent: 'space-between' }} align='baseline'>
+                  <Form.Item name={['price', 'min']}>
+                    <InputNumber placeholder='Min' style={{ width: '125px'}} controls={false}/>
+                  </Form.Item>
+                  <span>to</span>
+                  <Form.Item name={['price', 'max']}>
+                    <InputNumber placeholder='Max' style={{ width: '125px'}} controls={false}/>
+                  </Form.Item>
+                </Space>
+                <Form.Item>
+                  <div className='gradient-wrapper'>
+                    <Button htmlType='submit' className='apply-btn'>Apply</Button>
+                  </div>
+                </Form.Item>
+              </Form>
+            </Panel>
             <Panel
               key="attributes"
               header="Attributes filter"
