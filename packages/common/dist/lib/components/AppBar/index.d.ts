79b46db8802377e506b3acf245ae6891bb0a4990
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-02-16 23:59:16 +0900

    New Token Metadata Standard 1.1.0 v2 migration

--- packages/common/dist/lib/components/AppBar/index.d.ts
+++ packages/common/dist/lib/components/AppBar/index.d.ts
@@ -1,8 +1,7 @@
-/// <reference types="react" />
 export declare const AppBar: (props: {
-  left?: JSX.Element;
-  right?: JSX.Element;
-  useWalletBadge?: boolean;
-  additionalSettings?: JSX.Element;
+    left?: JSX.Element;
+    right?: JSX.Element;
+    useWalletBadge?: boolean;
+    additionalSettings?: JSX.Element;
 }) => JSX.Element;
 //# sourceMappingURL=index.d.ts.map
