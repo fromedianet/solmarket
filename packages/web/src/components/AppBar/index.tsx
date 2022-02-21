7ac64d77177c5547757d95d8a343f44850654ba5
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-02-22 00:18:19 +0900

    Implemented search creators

--- packages/web/src/components/AppBar/index.tsx
+++ packages/web/src/components/AppBar/index.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { MenuOutlined, SearchOutlined, UserOutlined } from '@ant-design/icons';
-import { ConnectButton } from '@oyster/common';
+import { Select } from 'antd';
+import { ConnectButton, shortenAddress, useMeta } from '@oyster/common';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { Link } from 'react-router-dom';
 import { useSetSidebarState } from '../../contexts';
 import { CurrentUserBadge } from '../CurrentUserBadge';
-import { SearchBar } from '../SearchBar';
+
+const { Option } = Select;
 
 export const WalletInfo = () => {
   const { connected } = useWallet();
@@ -43,12 +45,12 @@ export const LogoLink = () => {
 export const AppBar = () => {
   const { handleToggle } = useSetSidebarState();
   const [showSearchBar, toggleSearchBar] = useState(false);
-  const [searchKey, setSearchKey] = useState('');
+  const { whitelistedCreatorsByCreator } = useMeta();
+  const creators = Object.values(whitelistedCreatorsByCreator);
 
-  const handleSearch = (event: any) => {
-    const searchText = event?.target?.value;
-    setSearchKey(searchText);
-  };
+  const onChange = (value) => {
+    window.location.href = `/art/${value}`;
+  }
 
   return (
     <div className="navbar-expand-lg">
@@ -63,17 +65,20 @@ export const AppBar = () => {
             </div>
           )}
           <div className={`search-container ${!showSearchBar && 'tw-hidden'}`}>
-            <SearchBar
-              value={searchKey}
-              placeholder="Search Collections"
-              autoCapitalize="none"
-              autoComplete="off"
-              autoCorrect="off"
-              spellCheck="false"
-              controlClass="search-control"
-              onChange={handleSearch}
-              tabIndex={0}
-            />
+            <Select
+              className='search-control'
+              placeholder='Search Creators'
+              optionFilterProp='children'
+              onChange={onChange}
+              suffixIcon={<SearchOutlined />}
+            >
+              {creators.map((item, index) => 
+                <Option key={index} value={item.info.address}>
+                  <img src={`https://avatars.dicebear.com/api/jdenticon/${item.info.address}.svg`} className='creator-icon' alt={item.info.address}/>
+                  <span>{shortenAddress(item.info.address)}</span>
+                </Option>  
+              )}
+            </Select>
           </div>
         </div>
         <div className="right-container">
