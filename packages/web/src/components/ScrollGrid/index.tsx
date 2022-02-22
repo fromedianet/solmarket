4f9450f8e8f2a729942a4b29255f0f0893ceb9a8
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-02-22 12:40:09 +0900

    Implemented horizontal grid for popular collection

--- packages/web/src/components/ScrollGrid/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
-
-export const ScrollGrid = () => {
-  
-}
--- packages/web/src/views/home/components/SalesList.tsx
+++ packages/web/src/views/home/components/SalesList.tsx
@@ -8,11 +8,26 @@ import { HowToBuyModal } from '../../../components/HowToBuyModal';
 
 import { AuctionRenderCard } from '../../../components/AuctionRenderCard';
 import { AuctionViewState, useAuctions } from '../../../hooks';
+import { HorizontalGrid } from '../../../components/HorizontalGrid';
+import { useCollection } from '../../../hooks/useCollection';
+import { CollectionCard } from '../../collections/components/CollectionCard';
 
 export const SalesListView = () => {
   const { isLoading } = useMeta();
+  const collections = useCollection();
   const liveAuctions = useAuctions(AuctionViewState.Live);
   const endedAuctions = useAuctions(AuctionViewState.Ended);
+
+  const items = collections.map((it, index) => {
+    return (
+      <CollectionCard
+        key={index}
+        itemId={`${index}`}
+        pubkey={it.pubkey}
+        className="home-collection"
+      />
+    );
+  });
 
   return (
     <div className="main-area">
@@ -23,6 +38,10 @@ export const SalesListView = () => {
         actionComponent={<HowToBuyModal buttonClassName="secondary-btn" />}
         useBannerBg
       />
+      <div className="auction-section">
+        <span className="auction-title">Popular Collections</span>
+        <HorizontalGrid childrens={items} />
+      </div>
       <div className="auction-section">
         <span className="auction-title">Live Auctions</span>
         <Row style={{ width: '100%' }} gutter={[16, 16]}>
