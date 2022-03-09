c86317a016b036ed01da839fad45512e18bd1314
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-03-10 00:00:22 +0900

    Fixed TimeAgo error

--- packages/web/src/views/ExCollection/components/Activities.tsx
+++ packages/web/src/views/ExCollection/components/Activities.tsx
@@ -6,7 +6,8 @@ import TimeAgo from 'javascript-time-ago';
 import en from 'javascript-time-ago/locale/en.json';
 import { LAMPORTS_PER_SOL } from '@solana/web3.js';
 
-TimeAgo.addDefaultLocale(en);
+TimeAgo.setDefaultLocale(en.locale)
+TimeAgo.addLocale(en)
 // Create formatter (English).
 const timeAgo = new TimeAgo('en-US');
 
