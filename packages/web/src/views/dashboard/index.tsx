d0c30232e0e540c36695148861a09df8f26f1393
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-03-12 03:19:55 +0900

    check magic link key existing

--- packages/web/src/views/dashboard/index.tsx
+++ packages/web/src/views/dashboard/index.tsx
@@ -1,15 +1,40 @@
 import React from 'react';
 import { Dashboard } from './dashboard';
-import { Spin } from 'antd';
+import { Spin, Button } from 'antd';
 import { Login } from './login';
 import { useDashboard } from '../../contexts/dashboardProvider';
+import Router from 'next/router';
 
 export const DashboardView = () => {
   const { loading, user, isConfigured } = useDashboard();
 
   if (!isConfigured) {
-    return;
+    return (
+      <div className="not-configured">
+        <span>Magic Link publishableKey required</span>
+        <Button
+          onClick={() => {
+            Router.push('/');
+            Router.reload();
+          }}
+        >
+          Return
+        </Button>
+      </div>
+    );
   }
 
-  return <>{loading ? <Spin /> : user ? <Dashboard /> : <Login />}</>;
+  return (
+    <>
+      {loading ? (
+        <div className="load-container">
+          <Spin size="large" />
+        </div>
+      ) : user ? (
+        <Dashboard />
+      ) : (
+        <Login />
+      )}
+    </>
+  );
 };
