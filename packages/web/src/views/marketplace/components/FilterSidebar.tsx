2edc61e8559e3f81126ebafa92850255d5856f83
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-04-04 18:23:24 +0900

    update styles

--- packages/web/src/views/marketplace/components/FilterSidebar.tsx
+++ packages/web/src/views/marketplace/components/FilterSidebar.tsx
@@ -108,7 +108,7 @@ export const FilterSidebar = (props: {
           <Collapse
             expandIconPosition="right"
             className="filter-container"
-            defaultActiveKey={["status", "price", "attributes"]}
+            defaultActiveKey={['status', 'price', 'attributes']}
             expandIcon={({ isActive }) =>
               isActive ? (
                 <MinusOutlined className="filter-icon" />
