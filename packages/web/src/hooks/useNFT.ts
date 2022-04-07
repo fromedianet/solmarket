2988031cb02e96b75b96a630b6510022e374b923
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-04-07 11:04:42 +0900

    Get NFT info from onchain

--- packages/web/src/hooks/useNFT.ts
+++ packages/web/src/hooks/useNFT.ts
@@ -1,9 +1,12 @@
+import { useConnection } from '@oyster/common';
+import { ParsedAccountData, PublicKey } from '@solana/web3.js';
 import { useEffect, useState } from 'react';
 import { NFT, Transaction } from '../models/exCollection';
 import { useNFTsAPI } from './useNFTsAPI';
 import { useTransactionsAPI } from './useTransactionsAPI';
 
 export const useNFT = (mint: string) => {
+  const connection = useConnection();
   const [nft, setNFT] = useState<NFT>();
   const [transactions, setTransactions] = useState<Transaction[]>([]);
   const [loading, setLoading] = useState(false);
@@ -13,11 +16,20 @@ export const useNFT = (mint: string) => {
   useEffect(() => {
     if (loading) return;
     setLoading(true);
+
     getNftByMint(mint)
       // @ts-ignore
       .then((res: {}) => {
         if (res['data']) {
-          setNFT(res['data']);
+          const data = res['data'];
+          getTokenAccount(mint).then(res => {
+            if (res) {
+              setNFT({
+                ...data,
+                ...res,
+              });
+            }
+          });
         }
       })
       .finally(() => setLoading(false));
@@ -30,6 +42,24 @@ export const useNFT = (mint: string) => {
         }
       });
   }, [mint]);
+
+  async function getTokenAccount(mint: string) {
+    try {
+      const res1 = await connection.getTokenLargestAccounts(
+        new PublicKey(mint),
+      );
+      const token = res1.value[0].address;
+      const res2 = await connection.getParsedAccountInfo(new PublicKey(token));
+      const tokenAccountData = (res2.value?.data as ParsedAccountData).parsed;
+      const owner = tokenAccountData['info']['owner'];
+      return {
+        tokenAddress: token.toBase58(),
+        owner: owner,
+      };
+    } catch {
+      return null;
+    }
+  }
 
   return { nft, transactions, loading };
 };
