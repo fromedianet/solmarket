85e11a21a9da6e915a155b401740cc072b63a7cb
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-04-11 10:28:42 +0900

    set api endpoints

--- packages/web/src/constants/apis.ts
+++ packages/web/src/constants/apis.ts
@@ -1,5 +1,20 @@
 export const APIS = {
-  base_api_url: 'https://staging.api.papercity.io/api',
-  // base_api_url: 'http://localhost:5000/api',
-  base_others_api_url: 'https://staging.othersapi.papercity.io/api',
+  base_api_url:
+    process.env.NEXT_ENV == 'production'
+      ? 'https://api.papercity.io/api'
+      : process.env.NEXT_ENV == 'development'
+      ? 'https://staging.api.papercity.io/api'
+      : 'http://localhost:5000/api',
+  base_others_api_url:
+    process.env.NEXT_ENV == 'production'
+      ? 'https://othersapi.papercity.io/api'
+      : process.env.NEXT_ENV == 'development'
+      ? 'https://staging.othersapi.papercity.io/api'
+      : 'https://staging.othersapi.papercity.io/api',
+  socket_url:
+    process.env.NEXT_ENV == 'production'
+      ? 'https://api.papercity.io/api'
+      : process.env.NEXT_ENV == 'development'
+      ? 'https://staging.api.papercity.io'
+      : 'http://localhost:5000',
 };
