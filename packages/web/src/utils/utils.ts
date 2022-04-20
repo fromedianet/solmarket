7d9c3a5fa10f0f0726c927626b703086bc4c5021
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-04-20 12:54:16 +0900

    Group by collections for myItems and listedItems

--- packages/web/src/utils/utils.ts
+++ packages/web/src/utils/utils.ts
@@ -19,3 +19,17 @@ export const getDateStringFromUnixTimestamp = (unix: number) => {
   const str = date.toLocaleString();
   return str;
 };
+
+export const groupBy = (list, keyGetter) => {
+  const map = new Map();
+  list.forEach(item => {
+    const key = keyGetter(item);
+    const collection = map.get(key);
+    if (!collection) {
+      map.set(key, [item]);
+    } else {
+      collection.push(item);
+    }
+  });
+  return map;
+};
