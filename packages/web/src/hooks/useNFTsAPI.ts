2f81c348af968ce94595259dc02206709001951f
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-05-09 11:27:52 +0900

    Update getListedNFTsByQUery

--- packages/web/src/hooks/useNFTsAPI.ts
+++ packages/web/src/hooks/useNFTsAPI.ts
@@ -25,7 +25,13 @@ export const useNFTsAPI = () => {
    * @returns
    */
   async function getListedNftsByQuery(param: QUERIES) {
-    const queryBody = getQueryPrameter(param);
+    const pcQuery = getQueryPrameter(param);
+    const meQuery = getQueryParameterForMagicEden(param);
+    const queryBody = {
+      pc:  pcQuery,
+      me: meQuery,
+      sort: param.sort,
+    };
     const result = await runAPI(
       false,
       'post',
@@ -82,11 +88,65 @@ export const useNFTsAPI = () => {
 
     queries['sort'] = sortQuery;
 
-    const result = {
-      query: JSON.stringify(queries),
-    };
+    return JSON.stringify(queries);
+  }
 
-    return result;
+  function getQueryParameterForMagicEden(param: QUERIES) {
+    const queries = {
+      $skip: param.skip ? param.skip : 0,
+      $limit: 20,
+    };
+    const match = {};
+    match['collectionSymbol'] = param.symbol;
+    if (param.searchKey) {
+      match['$text'] = {
+        $search: param.searchKey,
+      };
+    }
+    if (param.min || param.max) {
+      const takerAmount = {};
+      if (param.min) {
+        takerAmount['$gte'] = param.min * LAMPORTS_PER_SOL;
+      }
+      if (param.max) {
+        takerAmount['$lte'] = param.max * LAMPORTS_PER_SOL;
+      }
+      match['takerAmount'] = takerAmount;
+    }
+
+    if (param.attributes && Object.keys(param.attributes).length > 0) {
+      const attrs: any[] = [];
+      Object.keys(param.attributes).forEach(key => {
+        // @ts-ignore
+        const subAttrs = param.attributes[key].map(val => ({
+          attributes: {
+            $elemMatch: {
+              trait_type: key,
+              value: val,
+            },
+          },
+        }));
+        attrs.push({ $or: subAttrs });
+      });
+
+      match['$and'] = attrs;
+    }
+
+    queries['$match'] = match;
+
+    const sortQuery = {};
+    if (param.sort === 2) {
+      sortQuery['takerAmount'] = 1;
+    } else if (param.sort === 3) {
+      sortQuery['takerAmount'] = -1;
+    }
+    sortQuery['createdAt'] = -1;
+
+    queries['$sort'] = sortQuery;
+
+    const queryStr = `?q=${JSON.stringify(queries)}`;
+
+    return encodeURI(queryStr);
   }
 
   /**
