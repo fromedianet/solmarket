533d5aaacbbd4a65470c2af74e9914064ed92e6f
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-06-05 23:45:52 +0900

    Clear all expired caches

--- packages/web/src/views/home/index.tsx
+++ packages/web/src/views/home/index.tsx
@@ -1,11 +1,16 @@
 import { Layout } from 'antd';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocalCache } from '../../hooks/useLocalCache';
 // import { useStore } from '@oyster/common';
 // import { useMeta } from '../../contexts';
 import { SalesListView } from './SalesList';
 // import { SetupView } from './setup';
 
 export const HomeView = () => {
+  const localCache = useLocalCache();
+  useEffect(() => {
+    localCache.clearAll().then(result => console.log('Clear all expired caches', result));
+  }, []);
   // const { isLoading, store } = useMeta();
   // const { isConfigured } = useStore();
 
