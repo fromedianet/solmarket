9301566aa122c34a6e9cd4f1a4e6e6ac9a29f024
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-06-11 01:26:25 +0900

    Add OpenGraph meta tags in homepage

--- packages/web/src/views/home/index.tsx
+++ packages/web/src/views/home/index.tsx
@@ -1,6 +1,5 @@
 import { Layout } from 'antd';
 import React, { useEffect } from 'react';
-import Head from 'next/head';
 import { useLocalCache } from '../../hooks/useLocalCache';
 import { SalesListView } from './SalesList';
 
@@ -12,19 +11,6 @@ export const HomeView = () => {
 
   return (
     <Layout>
-      <Head>
-        <meta property="og:title" content="PaperCity" />
-        <meta
-          property="og:description"
-          content="PaperCity is a marketplace aggregator for NFTs on the Solana platform, incorporating listings from all the most popular marketplaces as well as our own inventory"
-        />
-        <meta property="og:url" content="https://papercity.io" />
-        <meta
-          property="og:image"
-          content="https://papercity-bucket.s3.amazonaws.com/papercity_logo-02.png"
-        />
-        <meta property="og:type" content="article" />
-      </Head>
       <SalesListView />
     </Layout>
   );
